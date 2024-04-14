b971f129ba71a99e2b78baa5cfb7d345f8f91394
Omar-1000 <165605860+Omar-1000@users.noreply.github.com>
2024-04-15 01:46:46 +0300

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,29 @@
+o_sum=0
+e_sum=0
+p_sum=0
+
+for i in range(10) : 
+    x=int(input("please enter positive numbers :- ")) 
+    if x <=0 : 
+        x=int(input("error , positive only")) 
+    p_flag=False
+    for j in range (2,x) :
+        if x ==1: 
+            break
+        else: 
+            
+            if x % j ==0 : 
+                p_flag=True
+    if p_flag :
+        pass
+    else:
+        p_sum+=x
+            
+
+    if x%2==0 :
+        e_sum+=x
+    else:
+        o_sum+=x
+print("sum of odd" , o_sum)
+print("sum of even" , e_sum) 
+print("sum of prime" , p_sum)
